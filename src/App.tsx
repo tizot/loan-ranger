aa7e7fe9ae5092d2c5623598303b7076088e3306
Camille Masset <camille@masset.email>
2025-04-18 22:17:08 +0200

    refactor: refactor NumberField into NumberInput component

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import {
   NumberFieldLabel,
 } from './components/ui/number-field';
 import { type LoanOutput, computeCost } from './lib/calculator';
-import { Show } from 'solid-js';
+import { Show, splitProps } from 'solid-js';
+import type { NumberFieldRootProps } from '@kobalte/core/number-field';
 
 const InputSchema = v.object({
   principal: v.pipe(v.number(), v.minValue(0)),
@@ -69,86 +70,51 @@ function App() {
         <h2 class="text-lg font-semibold">Calculateur de prêt immobilier</h2>
       </header>
 
-      {/* TODO: use grid with 1 column on mobile and 2 columns on larger screens */}
-      <main class="flex flex-col md:flex-row items-center md:items-start gap-3">
-        <Card class="w-full">
+      <main class="flex flex-col md:flex-row md:justify-center items-center md:items-stretch gap-3">
+        <Card class="w-full max-w-md">
           <CardHeader>
             <CardTitle>Informations sur le prêt</CardTitle>
           </CardHeader>
-          <CardContent>
-            <form class="flex flex-col gap-2 w-full max-w-md" onSubmit={handleSubmit}>
-              <NumberField
-                class="flex w-full flex-col gap-2"
-                rawValue={inputs.principal}
-                onRawValueChange={(value) => setInputs('principal', value)}
-              >
-                <NumberFieldGroup>
-                  <NumberFieldLabel>Montant emprunté (principal)</NumberFieldLabel>
-                  <NumberFieldInput />
-                  <NumberFieldErrorMessage>Le principal doit être un nombre positif.</NumberFieldErrorMessage>
-                </NumberFieldGroup>
-              </NumberField>
-
-              <NumberField
-                class="flex w-full flex-col gap-2"
-                rawValue={inputs.annualRatePercentage}
-                onRawValueChange={(value) => setInputs('annualRatePercentage', value)}
-              >
-                <NumberFieldGroup>
-                  <NumberFieldLabel>Taux d'intérêt annuel (%)</NumberFieldLabel>
-                  <NumberFieldInput />
-                  <NumberFieldErrorMessage>
-                    Le taux d'intérêt doit être un nombre entre 0 et 100.
-                  </NumberFieldErrorMessage>
-                </NumberFieldGroup>
-              </NumberField>
-
-              <NumberField
-                class="flex w-full flex-col gap-2"
-                rawValue={inputs.months}
-                onRawValueChange={(value) => setInputs('months', value)}
-              >
-                <NumberFieldGroup>
-                  <NumberFieldLabel>Durée du prêt (mois)</NumberFieldLabel>
-                  <NumberFieldInput />
-                  <NumberFieldErrorMessage>La durée doit être un nombre entier positif.</NumberFieldErrorMessage>
-                </NumberFieldGroup>
-              </NumberField>
-
-              <NumberField
-                class="flex w-full flex-col gap-2"
-                rawValue={inputs.initialFees}
-                onRawValueChange={(value) => setInputs('initialFees', value)}
-              >
-                <NumberFieldGroup>
-                  <NumberFieldLabel>Frais de dossier initiaux</NumberFieldLabel>
-                  <NumberFieldInput />
-                  <NumberFieldErrorMessage>Les frais doivent être un nombre positif.</NumberFieldErrorMessage>
-                </NumberFieldGroup>
-              </NumberField>
-
-              <NumberField
-                class="flex w-full flex-col gap-2"
-                rawValue={inputs.insuranceCost}
-                onRawValueChange={(value) => setInputs('insuranceCost', value)}
-              >
-                <NumberFieldGroup>
-                  <NumberFieldLabel>Coût de l'assurance</NumberFieldLabel>
-                  <NumberFieldInput />
-                  <NumberFieldErrorMessage>Le coût doit être un nombre positif.</NumberFieldErrorMessage>
-                </NumberFieldGroup>
-              </NumberField>
-
-              <Button type="submit" class="mt-3">
-                Calculer
-              </Button>
-            </form>
+          <CardContent class="flex flex-col gap-2 w-full max-w-md">
+            <NumberInput
+              rawValue={inputs.principal}
+              onRawValueChange={(value) => setInputs('principal', value)}
+              label="Montant emprunté (principal)"
+              errorMessage="Le principal doit être un nombre positif."
+            />
+            <NumberInput
+              rawValue={inputs.annualRatePercentage}
+              onRawValueChange={(value) => setInputs('annualRatePercentage', value)}
+              label="Taux d'intérêt annuel (%)"
+              errorMessage="Le taux d'intérêt doit être un nombre entre 0 et 100."
+            />
+            <NumberInput
+              rawValue={inputs.months}
+              onRawValueChange={(value) => setInputs('months', value)}
+              label="Durée du prêt (mois)"
+              errorMessage="La durée doit être un nombre entier positif."
+            />
+            <NumberInput
+              rawValue={inputs.initialFees}
+              onRawValueChange={(value) => setInputs('initialFees', value)}
+              label="Frais de dossier initiaux"
+              errorMessage="Les frais doivent être un nombre positif."
+            />
+            <NumberInput
+              rawValue={inputs.insuranceCost}
+              onRawValueChange={(value) => setInputs('insuranceCost', value)}
+              label="Coût de l'assurance"
+              errorMessage="Le coût doit être un nombre positif."
+            />
+            <Button type="button" onClick={handleOnClick} disabled={!validation().success}>
+              Calculer
+            </Button>
           </CardContent>
         </Card>
 
         <Show when={result.output} keyed>
           {(output) => (
-            <Card class="w-full">
+            <Card class="w-full max-w-md">
               <CardHeader>
                 <CardTitle>Coût du prêt</CardTitle>
               </CardHeader>
@@ -180,6 +146,20 @@ function App() {
     </div>
   );
 }
+
+type NumberInputProps = NumberFieldRootProps & { label: string; errorMessage: string };
+const NumberInput = (props: NumberInputProps) => {
+  const [local, rest] = splitProps(props, ['label', 'errorMessage']);
+  return (
+    <NumberField {...rest} class="flex w-full flex-col gap-2">
+      <NumberFieldGroup>
+        <NumberFieldLabel>{local.label}</NumberFieldLabel>
+        <NumberFieldInput />
+        <NumberFieldErrorMessage>{local.errorMessage}</NumberFieldErrorMessage>
+      </NumberFieldGroup>
+    </NumberField>
+  );
+};
 
 const DataDisplay = ({ title, value }: { title: string; value: string }) => (
   <div class="w-full">
